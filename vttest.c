bcfb7bde12c93c23cb19061a3297b4aa234e7ab7
Michael Gehring <mg@ebfe.org>
2014-04-21 14:01:42 +0200

    vttest add blink/reverse attribs

--- vttest.c
+++ vttest.c
@@ -6,6 +6,7 @@
 #define BOLD 1
 #define ITALIC 3
 #define UNDERLINE 4
+#define BLINK 5
 #define REVERSE 7
 
 void csi_m(int n) {
@@ -16,19 +17,32 @@ int main(int argc, char *argv[]) {
 	for (int color = 0; color < 8; color++) {
 		csi_m(NORMAL);
 		csi_m(30 + color);
-		printf("color%d ", color);
+		printf("color%d", color);
 		csi_m(NORMAL);
+		putchar(' ');
 		csi_m(30 + color);
 		csi_m(BOLD);
-		printf("bold%d ", color);
+		printf("bold%d", color);
 		csi_m(NORMAL);
+		putchar(' ');
 		csi_m(30 + color);
 		csi_m(ITALIC);
-		printf("italic%d ", color);
+		printf("italic%d", color);
 		csi_m(NORMAL);
+		putchar(' ');
 		csi_m(30 + color);
 		csi_m(UNDERLINE);
-		printf("underlined%d\n", color);
+		printf("underlined%d", color);
+		csi_m(NORMAL);
+		putchar(' ');
+		csi_m(30 + color);
+		csi_m(BLINK);
+		printf("blink%d", color);
+		csi_m(NORMAL);
+		putchar(' ');
+		csi_m(30 + color);
+		csi_m(REVERSE);
+		printf("reverse%d\n", color);
 	}
 	csi_m(NORMAL);
 
